4918f17d487b610b21c6789c32103299e7b476be
chetansirohi <35464633+chetansirohi@users.noreply.github.com>
2022-08-14 19:08:44 +0530

    Creating Separate Directory

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+const Navbar = ({ web3Handler, account }) => {
+    return (
+        <nav className="flex-between">
+
+            {account ? (
+                <a
+                    href={`${""}/address/${account}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="button">
+                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                </a>
+            ) : (
+                <button onClick={web3Handler} className="button">Connect Wallet</button>
+            )}
+        </nav>
+    )
+}
+
+export default Navbar;
